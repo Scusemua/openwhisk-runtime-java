08fdb7bcb6aa6febbbf6b7293f3d54c9891900af
Scusemua <benrcarver@gmail.com>
2021-08-27 14:33:27 -0400

    Fixed some bugs in that copy script

--- copy-files.sh
+++ copy-files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cp /home/ubuntu/repos/hops/hops-metadata-dal-impl-ndb/target/hops-metadata-dal-impl-ndb-3.2.0.3-SNAPSHOT /home/ubuntu/repos/openwhisk-runtime-java/core/java/hops-metadata-dal-impl-ndb-3.2.0.3-SNAPSHOT
-cp /home/ubuntu/repos/hops/hadoop-common-project/hadoop-common/target/hadoop-common-3.2.0.3-SNAPSHOT.jar /home/ubuntu/repos/openwhisk-runtime-java/core/java/libs/hadoop-common-3.2.0.3-SNAPSHOT.jar
+cp /home/ubuntu/repos/hops/hops-metadata-dal-impl-ndb/target/hops-metadata-dal-impl-ndb-3.2.0.3-SNAPSHOT.jar /home/ubuntu/repos/openwhisk-runtime-java/core/java8/hops-metadata-dal-impl-ndb-3.2.0.3-SNAPSHOT
+cp /home/ubuntu/repos/hops/hadoop-common-project/hadoop-common/target/hadoop-common-3.2.0.3-SNAPSHOT.jar /home/ubuntu/repos/openwhisk-runtime-java/core/java8/libs/hadoop-common-3.2.0.3-SNAPSHOT.jar
