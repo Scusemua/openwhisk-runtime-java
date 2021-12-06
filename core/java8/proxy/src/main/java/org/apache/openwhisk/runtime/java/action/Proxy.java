320cc848dc123ae25467d5636180b9d310591e6a
Scusemua <benrcarver@gmail.com>
2021-12-05 21:05:46 -0500

    Print heap memory when receiving invocation and when invocation terminates.

--- core/java8/proxy/src/main/java/org/apache/openwhisk/runtime/java/action/Proxy.java
+++ core/java8/proxy/src/main/java/org/apache/openwhisk/runtime/java/action/Proxy.java
@@ -126,6 +126,11 @@ public class Proxy {
     private static void writeLogMarkers() {
         System.out.println("XXX_THE_END_OF_A_WHISK_ACTIVATION_XXX");
         System.err.println("XXX_THE_END_OF_A_WHISK_ACTIVATION_XXX");
+
+        System.out.println("Heap Memory In-Use: " + (Runtime.getRuntime().totalMemory() / 1000000.0) +
+                " MB\nHeap Memory Free/Available: " + (Runtime.getRuntime().freeMemory() / 1000000.0) +
+                " MB.");
+
         System.out.flush();
         System.err.flush();
     }
@@ -191,7 +196,9 @@ public class Proxy {
                 return;
             }
 
-            System.out.println("RECEIVED INVOCATION.");
+            System.out.println("RECEIVED INVOCATION. Heap Memory In-Use: " + (Runtime.getRuntime().totalMemory() / 1000000.0) +
+                    " MB\nHeap Memory Free/Available: " + (Runtime.getRuntime().freeMemory() / 1000000.0) +
+                    " MB.");
 
             ClassLoader cl = Thread.currentThread().getContextClassLoader();
             
