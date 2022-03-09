abdc83ae652590b4d87047d660f341d9dc2dcf7f
Scusemua <benrcarver@gmail.com>
2022-03-08 21:24:00 -0500

    Removed a bunch of files and moved stuff around for Nuclio version.

--- copy-files.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp /home/ubuntu/repos/hops/hops-metadata-dal-impl-ndb/target/hops-metadata-dal-impl-ndb-3.2.0.3-SNAPSHOT.jar /home/ubuntu/repos/openwhisk-runtime-java/core/java8/hops-metadata-dal-impl-ndb-3.2.0.3-SNAPSHOT.jar
-cp /home/ubuntu/repos/hops/hadoop-common-project/hadoop-common/target/hadoop-common-3.2.0.3-SNAPSHOT.jar /home/ubuntu/repos/openwhisk-runtime-java/core/java8/libs/hadoop-common-3.2.0.3-SNAPSHOT.jar
